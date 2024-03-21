b6d52f758314a60d32f95ac61e70db95f206a2c6
xMarioOP <marioalejandro424@outlook.com>
2024-03-20 23:17:49 -0500

    Comments

---
+++ comments/script.js
@@ -0,0 +1,18 @@
+
+//COMMENTS:
+/* These are lines of text that are not executed as code. 
+ *They serve to document the code, explain its operation or leave notes for other developers.   */
+
+
+// This is a one line comment
+
+/* this is a longer,
+ * multi-line comment
+ */
+
+
+
+/* You can't, however, /* nest comments */ SyntaxError */
+
+//In this case, you need to break up the */ pattern. For example, by inserting a backslash:
+/* You can /* nest comments *\/ by escaping slashes */
