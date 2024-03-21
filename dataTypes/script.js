a2ae3d7ab42419d386d47c484293791c14fdb5a3
xMarioOP <marioalejandro424@outlook.com>
2024-03-21 18:18:47 -0500

    Data types

---
+++ dataTypes/script.js
@@ -0,0 +1,20 @@
+/* 
+The latest ECMAScript standard defines eight data types:
+
+Seven data types that are primitives:
+Boolean ====> true and false.
+null ====> A special keyword denoting a null value. (Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant.)
+undefined ====> A top-level property whose value is not defined.
+Number ====> An integer or floating point number. For example: 42 or 3.14159.
+BigInt====> An integer with arbitrary precision. For example: 9007199254740992n.
+String ====> A sequence of characters that represent a text value. For example: "Howdy".
+Symbol ====> A data type whose instances are unique and immutable.
+
+*/
+
+let number = [7, 3.14, 19.95, 2.998e8, -1]  //Examples of numbers
+
+let string = 'You are learning JavaScript'                 
+let string2 =  "You're going to like JavaScript"  //text strings can are represented by single or double quotation marks or grave accents
+let string3 =  `This is a text string`
+
