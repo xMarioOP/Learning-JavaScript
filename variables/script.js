0f86e5fd3099f3da156732383e2ba042c556c6f7
xMarioOP <marioalejandro424@outlook.com>
2024-03-24 11:38:09 -0500

    Variables

---
+++ variables/script.js
@@ -0,0 +1,19 @@
+/*A variable is a container for storing data. This data can be of various types
+ such as numbers, strings, booleans, objects, functions, and so on. 
+ Variables in JavaScript can be declared using the keywords var, let, or const. Here is an explanation of each:
+*/
+
+//To create a variable we can use the reserved word let and give the variable a name. For example:
+let number = 1 
+let string = "Hello World"
+let boolean = true
+
+
+//Constants are variables that cannot be reassigned. To create a constant, we use the reserved word const:
+const PI = 3.1415
+const number2 = 10
+
+
+/*var is no longer used due to its function scope and hoisting behavior, which can cause confusion and errors. 
+Instead, let and const offer clearer block scope and do not have hoisting behavior, which makes the code more 
+predictable and less error-prone.*/
