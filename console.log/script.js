4b5b74bc4cf3d50cb0abb5e42a4d1b8680aec498
xMarioOP <marioalejandro424@outlook.com>
2024-03-27 17:56:35 -0500

    Console.log()

---
+++ console.log/script.js
@@ -0,0 +1,20 @@
+/*
+console.log() is a function in JavaScript that is used to print log messages to the browser console or the console 
+of a JavaScript runtime environment, such as Node.js. It is primarily used for debugging and checking the status of an 
+application while it is under development.
+When console.log() is called, any type of data can be passed as an argument, such as strings, numbers, objects, arrays, etc.
+*/
+ 
+console.log('Hello, JavaScript') // -> 'Hello, JavaScript'.
+ 
+const name = 'JavaScript' 
+console.log(name) // -> 'JavaScript'
+ 
+
+//You can display several messages and variable values in the same console.log() by separating them by commas.
+const number = 'JavaScript' 
+const version = 2023 
+console.log(number, version) // -> 'JavaScript 2023'
+ 
+
+console.log(42) // - > '42'
