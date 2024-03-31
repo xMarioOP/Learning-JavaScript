c7e1c403e3b16a17137f80916e2774f8884edfc6
xMarioOP <marioalejandro424@outlook.com>
2024-03-31 17:04:36 -0500

    JavaScript statements and expressions

---
+++ javaScriptStatementsAndExpressions/script.js
@@ -0,0 +1,29 @@
+/**
+ Declarations:
+    -Statements in JavaScript are instructions that define variables, functions, objects, or other code components.
+ */
+    let x; // Variable declaration statement
+    if (condition) { // If statement
+        // code block
+    }
+
+    for (let i = 0; i < 5; i++) { // For loop statement
+        // code block
+    }
+
+    function myFunction() { // Function declaration statement
+        // function body
+    }
+
+
+/**
+ Expressions:
+    -Expressions in JavaScript are pieces of code that produce a value.
+    -Expressions can be simple, like a literal value, a variable, or a function call, or more complex, involving operators and function invocations.
+    -Expressions can be standalone or part of larger statements.
+ */
+    5; // Literal expression
+    x; // Variable expression
+    2 + 3; // Arithmetic expression
+    myFunction(); // Function call expression
+    
