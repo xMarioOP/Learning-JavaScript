3b4af57170883cc51c6f0276f0c5248a86845df8
xMarioOP <marioalejandro424@outlook.com>
2024-06-27 17:29:49 -0500

    Strings

---
+++ strings/script.js
@@ -0,0 +1,16 @@
+let hello = "Hello"
+let world = "World"
+let phrase = hello + " " + world + "!"
+console.log(phrase);
+
+// Template literals
+let phrase2 = `${hello} ${world}!`
+console.log("With template literals: ",phrase2);
+
+// Multi-line strings
+let text = "Text1... \n " + "Text2..."
+console.log(text);
+
+let text2 = `Text1...
+Text2...`
+console.log("Wiht template literals: ",text2);
